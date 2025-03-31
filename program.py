16d4d0f95a41f787d27e7e58f728621259bcddc2
rileyroycestevens <rileyroycestevens@gmail.com>
2025-03-30 20:42:28 -0500

    Added color prompt and some input validation

--- program.py
+++ program.py
@@ -1,16 +1,19 @@
 class Car:
-    def __init__(self, make: str, model: str, year: int):
+    def __init__(self, make: str, model: str, year: int, color: str):
         self.make = make
         self.model = model
         self.year = year
+        self.color = color
         self.__mileage = 0
 
     @property
     def miles(self):
+        """Getter for mileage."""
         return self.__mileage
 
     @miles.setter
     def miles(self, miles):
+        """Setter for mileage."""
         if miles < 0:
             raise ValueError("Miles cannot be less than 0")
         self.__mileage = miles
@@ -18,20 +21,35 @@ class Car:
 cars = []
 
 def main():
-    try:
-        make = input("Make: ")
-        model = input("Model: ")
-        year = int(input("Year: "))
-        mileage = int(input("Miles: "))
-    except ValueError:
-        print("Positive whole numbers only.")
-    
-    new_car = Car(make, model, year)
-    new_car.miles = mileage
-    cars.append(new_car)
+    while True:
+        try:
+            make = input("Make: ").title().strip()
 
-    print(f"{new_car.make} | {new_car.model} | {new_car.year} | {new_car.miles}")
+            model = input("Model: ").title().strip()
+
+            year = int(input("Year: ").strip())
+            if year < 1800 or year > 2026:
+                raise Exception("Impossible year!")
+            
+            color = input("Color: ").lower().strip()
+
+            mileage = int(input("Miles: ").strip())
+            break
+        except ValueError:
+            print("Positive whole numbers only.")
+    
+    new_car = Car(make, model, year, color)
+    new_car.miles = mileage
+
+    cars.append(new_car)
+    print(f"{new_car.make} | {new_car.model} | {new_car.year} | {new_car.color} | {new_car.miles}")
     
 
 if __name__ == "__main__":
     main()
+
+# getter and setter name must be the same
+# Private variable doesn't go in class parameters
+
+# When you call them, setter has an '=' after, while getter is just mentioned.
+# ex. new_car.miles = 300 (setter), new_car.miles (getter)
